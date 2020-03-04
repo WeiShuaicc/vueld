754944ac5c2f6224c847306c69b693f41c5f1c90
WeiShuaicc <1538081244@qq.com>
2020-03-04 22:41:50 +0800

    添加路由设置

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>这是一个商品列表页面</div>
+    <span>{{$route.params.goodsId}}</span><br>
+    <span>{{$route.params.user}}</span>
+  </div>
+</template>
+<style></style>
+<script>
+    export default {
+      data(){
+        return{
+          msg:'hello'
+        }
+      }
+     }
+</script>
+
