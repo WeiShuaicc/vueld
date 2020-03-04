01722ebd5c460bde81e381055410ab88dd8e0296
WeiShuaicc <1538081244@qq.com>
2020-03-04 23:46:06 +0800

    动态切换

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,36 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '@/views/GoodsList'
-
+import Title from "@/views/Title";
+import Image from "@/views/Image";
+import Cart from "@/views/Cart";
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
-      path: '/goods/goodsId/user/:user',
+      path: '/goods',
       name: 'GoodsList',
-      component: GoodsList
-    }
+      component: GoodsList,
+      children:[
+        {
+          path: 'title',
+          name: 'title',
+          component:Title
+        },
+        {
+          path: 'img',
+          name: 'img',
+          component:Image
+        }
+        ]},
+  {
+      path: '/cart',
+      name: 'cart',
+      component:Cart
+        }
+
+
   ]
 })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -3,6 +3,14 @@
     <div>这是一个商品列表页面</div>
     <span>{{$route.params.goodsId}}</span><br>
     <span>{{$route.params.user}}</span>
+    <router-link to="/goods/title">显示商品标题</router-link>
+    <router-link to="/goods/img">显示商品图片</router-link>
+
+    <div>
+    <router-view></router-view>
+    </div>
+    <router-link to="/cart">跳转到购物车页面</router-link>
+    <button @click="jump">button - 跳转到购物车页面</button>
   </div>
 </template>
 <style></style>
@@ -12,7 +20,14 @@
         return{
           msg:'hello'
         }
+      },
+      methods:{
+        jump(){
+          this.$router.push({path:'/cart?goodid=123'})
+          // this.$router.go(-2)  //支持倒退与前进
+    }
       }
+
      }
 </script>
 
