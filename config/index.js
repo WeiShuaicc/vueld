b22d10ea0ff1659ef460e264a3278d2dcd3686d6
WeiShuaicc <1538081244@qq.com>
2020-03-11 22:36:40 +0800

    前端应用

--- config/index.js
+++ config/index.js
@@ -6,15 +6,24 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
-    // Paths
+    env: require('./dev.env'),
+    port: 8989,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/goods':{
+        target:'http://localhost:3000'
+      },
+      '/goods/*':{
+        target:'http://localhost:3000'
+      },
+      '/users/*':{
+        target:'http://localhost:3000'
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
---
+++ demo/views/GoodsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>这是一个商品列表页面</div>
+    <span>{{$route.params.goodsId}}</span><br>
+    <span>{{$route.params.user}}</span>
+    <router-link to="/goods/title">显示商品标题</router-link>
+    <router-link to="/goods/img">显示商品图片</router-link>
+
+    <div>
+    <router-view></router-view>
+    </div>
+    <router-link v-bind:to="{name:'cart',params:{cartid:134}}">跳转到购物车页面</router-link>
+  </div>
+</template>
+<style></style>
+<script>
+    export default {
+      data(){
+        return{
+          msg:'hello'
+        }
+      },
+      methods:{
+        jump(){
+          this.$router.push({path:'/cart?goodid=123'})
+          // this.$router.go(-2)  //支持倒退与前进
+    }
+      }
+
+     }
+</script>
+
