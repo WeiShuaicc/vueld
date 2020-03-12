2888395245a8601163bbf48dfb3f0d53a9fa0156
WeiShuaicc <1538081244@qq.com>
2020-03-12 23:18:44 +0800

    路由器设置

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,13 @@ import OrderSuccess from './../views/OrderSuccess'
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+  //mode:'history', 注意只在使用前端路由时开启 用于去除hash模式下的URL路径中#
   routes: [
     {
       path: '/',
       name: 'GoodsList',
-      component: GoodsList,
-      },
+      component:GoodsList
+    },
     {
       path: '/cart',
       name: 'Cart',
